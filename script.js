71a8e02dae6eb87fa4edac274c754fd233e8a364
Roberto Alagemovits <robertomovits@gmail.com>
2023-08-16 03:21:59 -0300

    arregle el css

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-/* ;) */
+const opciones = document.querySelector('.opciones');
+const burgerIcon = document.querySelector('.burger-icon');
+
+burgerIcon.addEventListener('click', () => {
+    opciones.classList.toggle('active');
+});
+
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (event) => {
+    const name = form.querySelector('#nombre');
+    const email = form.querySelector('#email');
+    const mensaje = form.querySelector('#mensaje');
+    
+    if (!name.value || !email.value || !mensaje.value) {
+        event.preventDefault();
+        alert('Please fill in all fields.');
+    }
+});
+
